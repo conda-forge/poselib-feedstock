f383c46cbe50ebc7c76a8d6b96584442fe112664
Tobias Fischer <info@tobiasfischer.info>
2024-08-25 08:05:09 +1000

    Create bld.bat

---
+++ recipe/bld.bat
@@ -0,0 +1,13 @@
+@echo on
+
+mkdir build
+cd build
+
+cmake -G Ninja -DBUILD_SHARED_LIBS=ON -DPYTHON_PACKAGE=ON ..
+if %ERRORLEVEL% neq 0 exit 1
+
+cmake --build . --config Release
+if %ERRORLEVEL% neq 0 exit 1
+
+cmake --build . --config Release --target install
+if %ERRORLEVEL% neq 0 exit 1
