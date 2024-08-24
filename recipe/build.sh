d8250203fd2828b72d72b20246e8ab459a96b6b4
Tobias Fischer <info@tobiasfischer.info>
2024-08-25 07:28:36 +1000

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,10 @@
 mkdir build
 cd build
 
-cmake ${CMAKE_ARGS} -G Ninja ..
+cmake ${CMAKE_ARGS} -G Ninja -DBUILD_SHARED_LIBS=ON -DBUILD_PYTHON=ON ..
 
 cmake --build . --config Release -- -j$CPU_COUNT
 cmake --build . --config Release --target install
 
-cd ..
-
-$PYTHON -m pip install . -vv --no-build-isolation
+# cd ..
+# $PYTHON -m pip install . -vv --no-build-isolation
