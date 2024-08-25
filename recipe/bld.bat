6d92a5b4d708de102d4444fefaab38f83b15c412
Tobias Fischer <info@tobiasfischer.info>
2024-08-25 10:19:50 +1000

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,7 +3,7 @@
 mkdir build
 cd build
 
-cmake -G Ninja -DBUILD_SHARED_LIBS=ON -DPYTHON_PACKAGE=ON -DPython_EXECUTABLE=%PYTHON% ..
+cmake -G Ninja -DPYTHON_PACKAGE=ON -DPython_EXECUTABLE=%PYTHON% ..
 if %ERRORLEVEL% neq 0 exit 1
 
 cmake --build . --config Release
